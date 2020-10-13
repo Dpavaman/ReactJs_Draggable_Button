eec1fa12e74cdf699543a4a72537115c6902291b
Dpavaman <67939138+Dpavaman@users.noreply.github.com>
2020-10-13 11:01:57 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Draggable from 'react-draggable';
 
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [type, setType] = useState('')
+  // const [coordinates, setCoordinates] = useState('')
+
+  const dragHandler = (e, ui) => {
+    setX(x + ui.deltaX);
+    setY(x + ui.deltaY)
+  }
+  const eventHandler = (work, coordinates) => {
+    setType(work.type);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Draggable Button</h1>
+      <h2>Button is Posotioned at x: {x.toFixed(0)} and Y : {y.toFixed(0)} coordinates </h2>
+      <div>
+        Operation last carried : {type.toUpperCase()}
+      </div>
+      <Draggable bounds={{ top: 0, left: 0 }} onDrag={dragHandler} onStart={eventHandler} onStop={eventHandler} onMouseDown={eventHandler}
+        onMouseUp={eventHandler} onTouchStart={eventHandler} onTouchEnd={eventHandler} >
+        <button className='draggable'>I am Ready to be dragged </button>
+      </Draggable>
     </div>
   );
 }
